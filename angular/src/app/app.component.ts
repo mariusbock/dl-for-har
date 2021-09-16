682e559b5974ff94766db5de5f20dc3fab389089
Alexander Hölzemann <alexander.hoelzemann@student.uni-siegen.de>
2021-09-16 16:31:18 +0200

    update

--- angular/src/app/app.component.ts
+++ angular/src/app/app.component.ts
@@ -7,4 +7,31 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'webpage';
+  config: any;
+  fullpage_api: any;
+
+  constructor() {
+
+    // for more details on config options please visit fullPage.js docs
+    this.config = {
+
+      // fullpage options
+      licenseKey: 'YOUR LICENSE KEY HERE',
+      anchors: ['firstPage', 'secondPage', 'thirdPage', 'fourthPage', 'lastPage'],
+      menu: '#menu',
+
+      // fullpage callbacks
+      afterResize: () => {
+        console.log("After resize");
+      },
+      afterLoad: (origin: any, destination: any, direction: any) => {
+        console.log(origin.index);
+      }
+    };
+  }
+
+  getRef(fullPageRef: any) {
+    this.fullpage_api = fullPageRef;
+  }
+
 }
--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -3,6 +3,7 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AngularFullpageModule } from '@fullpage/angular-fullpage';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    AngularFullpageModule
   ],
   providers: [],
   bootstrap: [AppComponent]
